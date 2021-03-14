003320b8232e3c1dbd542732cad9c50944a09757
Ma3querader <593661332@qq.com>
2021-03-14 17:40:22 +0800

    命令模式

---
+++ src/main/java/com/command/Command.java
@@ -0,0 +1,9 @@
+package com.command;
+
+/**
+ * @Author: panyusheng
+ * @Date: 2021/3/14
+ * @Version 1.0
+ */
+public interface Command {
+}
---
+++ src/main/java/com/command/Test.java
@@ -0,0 +1,9 @@
+package com.command;
+
+/**
+ * @Author: panyusheng
+ * @Date: 2021/3/13
+ * @Version 1.0
+ */
+public class Test {
+}
